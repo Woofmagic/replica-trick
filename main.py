46c4a56e79ba6542312b5ba7646f0e010ae02ac6
MidnightExcursion <dima.watkins@gmail.com>
2025-03-07 13:42:52 -0600

    incorporated pysr into main

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Concatenate, Dense, Lambda
 from tensorflow.keras.models import Model
 
+import pysr
+from pysr import PySRRegressor
+
 from app.utilities.plotting.plot_customizer import PlotCustomizer
 
 from app.extractions.network_builder import NetworkBuilder, WeightsBiasesCallback
@@ -33,6 +36,79 @@ from app.data.experimental.experiment import conduct_experiment
 
 from app.extractions.replica_method import run_replica_method
 
+_SEARCH_SPACE_BINARY_OPERATORS = [
+    "+", "-", "*", "/", "^"
+]
+
+_SEARCH_SPACE_UNARY_OPERATORS = [
+   "exp", "log", "sqrt", "sin", "cos", "tan"
+]
+
+_SEARCH_SPACE_MAXIUMUM_COMPLEXITY = 20
+
+_SEARCH_SPACE_MAXIMUM_DEPTH = None
+
+py_regressor_models = PySRRegressor(
+
+    # === SEARCH SPACE ===
+
+    # (1): Binary operators:
+    binary_operators = _SEARCH_SPACE_BINARY_OPERATORS,
+
+    # (2): Unary operators:
+    unary_operators = _SEARCH_SPACE_UNARY_OPERATORS,
+
+    # (3): Maximum complexity of chosen equation:
+    maxsize = _SEARCH_SPACE_MAXIUMUM_COMPLEXITY,
+
+    # (4): Maximum depth of a chosen equation:
+    maxdepth = _SEARCH_SPACE_MAXIMUM_DEPTH,
+
+    # === SEARCH SIZE ===
+
+    # (1): Number of iterations for the algorithm:
+    niterations = 40,
+
+    # (2): The number of "populations" running:
+    populations = 15,
+
+    # (3): The size of each population:
+    population_size = 33,
+
+    # (4): Whatever the fuck this means:
+    ncycles_per_iteration = 550,
+
+    # === OBJECTIVE ===
+
+    # (1): Option 1: Specify *Julia* code to compute elementwise loss:
+    elementwise_loss = "loss(prediction, target) = (prediction - target)^2",
+
+    # (2): Option 2: Code your own *Julia* loss:
+    loss_function = None,
+
+    # (3): Choose the "metric" to select the final function --- can be 'accuracy,' 'best', or 'score':
+    model_selection = 'best',
+
+    # (4): How much to penalize a given function if dim-analysis doesn't work:
+    dimensional_constraint_penalty = 1000.0,
+
+    # (5): Enable or disable a search for dimensionless constants:
+    dimensionless_constants_only = False,
+
+    # === COMPLEXITY ===
+
+    # (1): Multiplicative factor that penalizes a complex function:
+    parsimony = 0.0032,
+
+    # (2): A complicated dictionary governing how complex a given operation can be:
+    constraints = None,
+
+    # (3): Another dictionary that enforces the number of times an operator may be nested:
+    nested_constraints = None,
+
+    # (4): Another dictionary that limits the complexity per operator:
+    complexity_of_operators = None)
+
 def run():
 
     print(f"> Now running TensorFlow Version {tf.version.VERSION}")
@@ -41,11 +117,11 @@ def run():
     kinematic_set_integer = 1
 
     # (2): The number of replicas to use.
-    number_of_replicas = 300
+    number_of_replicas = 20
 
     # run_replica_method(kinematic_set_integer, number_of_replicas)
 
-    training_x_data_7, training_y_data_7 = conduct_experiment()
+    training_x_data, training_y_data = conduct_experiment()
 
     # (1): Begin iterating over the replicas:
     for replica_index in range(number_of_replicas):
@@ -79,15 +155,15 @@ def run():
         print(f"> Replica #{replica_index + 1} now running...")
 
         history_of_training_7 = tensorflow_network.fit(
-            training_x_data_7, 
-            training_y_data_7, 
+            training_x_data, 
+            training_y_data, 
             epochs = 2000)
 
         # (3): Construct the loss plot:
         training_loss_data_7 = history_of_training_7.history['loss']
-        model_predictions_7 = tensorflow_network.predict(training_x_data_7)
+        model_predictions_7 = tensorflow_network.predict(training_x_data)
 
-        tensorflow_network.save(f"replica_number_{replica_index + 1}_v4.keras")
+        tensorflow_network.save(f"replica_number_{replica_index + 1}_v5.keras")
 
         print(f"> Saved replica!" )
         print(f"> Replica #{replica_index + 1} finished running...")
@@ -104,9 +180,9 @@ def run():
         
         plot_customization_nn_loss = PlotCustomizer(
             axis_instance_nn_loss,
-            title = r"Neural Network Loss",
+            title = r"Neural Network Loss per Epoch",
             xlabel = r"Epoch",
-            ylabel = r"Loss")
+            ylabel = r"Loss (MSE)")
         
         plot_customization_nn_loss.add_line_plot(
             x_data = np.arange(0, 2000, 1),
@@ -127,19 +203,19 @@ def run():
             ylabel = r"f(x)")
 
         plot_customization_data_comparison.add_scatter_plot(
-            x_data = training_x_data_7,
-            y_data = training_y_data_7,
+            x_data = training_x_data,
+            y_data = training_y_data,
             label = r'Experimental Data',
             color = "red")
         
         plot_customization_data_comparison.add_scatter_plot(
-            x_data = training_x_data_7,
+            x_data = training_x_data,
             y_data = model_predictions_7,
             label = r'Model Predictions',
             color = "orange")
         
-        figure_instance_nn_loss.savefig(f"loss_v{replica_index+1}_v4")
-        figure_instance_fitting.savefig(f"fitting{replica_index+1}_v4")
+        figure_instance_nn_loss.savefig(f"loss_v{replica_index+1}_v5")
+        figure_instance_fitting.savefig(f"fitting{replica_index+1}_v5")
 
     model_paths = [os.path.join(os.getcwd(), file) for file in os.listdir(os.getcwd()) if file.endswith("v4.keras")]
     models = [tf.keras.models.load_model(path) for path in model_paths]
@@ -176,7 +252,7 @@ def run():
 
         return y_mean, y_min, y_max, y_percentile_10, y_percentile_20, y_percentile_30, y_percentile_40, y_percentile_60, y_percentile_70, y_percentile_80, y_percentile_90
 
-    y_mean, y_min, y_max, y_percentile_10, y_percentile_20, y_percentile_30, y_percentile_40, y_percentile_60, y_percentile_70, y_percentile_80, y_percentile_90 = predict_with_models(models, training_x_data_7)
+    y_mean, y_min, y_max, y_percentile_10, y_percentile_20, y_percentile_30, y_percentile_40, y_percentile_60, y_percentile_70, y_percentile_80, y_percentile_90 = predict_with_models(models, training_x_data)
 
     # (1): Set up the Figure instance
     figure_instance_predictions = plt.figure(figsize = (18, 6))
@@ -191,14 +267,14 @@ def run():
         ylabel = r"$f(x)$")
     
     plot_customization_predictions.add_line_plot(
-        x_data = training_x_data_7,
+        x_data = training_x_data,
         y_data = y_mean,
         label = r'Replica Average',
         color = "blue",
         linestyle = "-")
     
     plot_customization_predictions.add_fill_between_plot(
-        x_data = training_x_data_7,
+        x_data = training_x_data,
         lower_y_data = y_min,
         upper_y_data = y_max,
         label = r'Min/Max Bound',
@@ -206,7 +282,7 @@ def run():
         alpha = 0.2)
     
     plot_customization_predictions.add_fill_between_plot(
-        x_data = training_x_data_7,
+        x_data = training_x_data,
         lower_y_data = y_percentile_10,
         upper_y_data = y_percentile_90,
         label = r'10/90 Bound',
@@ -214,7 +290,7 @@ def run():
         alpha = 0.25)
 
     plot_customization_predictions.add_fill_between_plot(
-        x_data = training_x_data_7,
+        x_data = training_x_data,
         lower_y_data = y_percentile_20,
         upper_y_data = y_percentile_80,
         label = r'20/80 Bound',
@@ -222,7 +298,7 @@ def run():
         alpha = 0.3)
     
     plot_customization_predictions.add_fill_between_plot(
-        x_data = training_x_data_7,
+        x_data = training_x_data,
         lower_y_data = y_percentile_30,
         upper_y_data = y_percentile_70,
         label = r'30/70 Bound',
@@ -230,7 +306,7 @@ def run():
         alpha = 0.35)
     
     plot_customization_predictions.add_fill_between_plot(
-        x_data = training_x_data_7,
+        x_data = training_x_data,
         lower_y_data = y_percentile_40,
         upper_y_data = y_percentile_60,
         label = r'40/60 Bound',
@@ -238,16 +314,18 @@ def run():
         alpha = 0.4)
     
     plot_customization_predictions.add_scatter_plot(
-            x_data = training_x_data_7,
-            y_data = training_y_data_7,
+            x_data = training_x_data,
+            y_data = training_y_data,
             label = r'Experimental Data',
             color = "red",
             marker = 'o',
             markersize = 1.)
     
-    figure_instance_predictions.savefig(f"replica_average_data_v4")
+    figure_instance_predictions.savefig(f"replica_average_data_v5")
+    plt.close()
 
-
+    py_regressor_models.fit(training_x_data, y_mean)
+    py_regressor_models.sympy()
 
 if __name__ == "__main__":
     run()
